cc97d51931e9fa18bc9c7455864c4886767a81a6
SAHIL GAIKWAD <95294447+SahilGaikwad@users.noreply.github.com>
2022-01-19 22:44:09 +0530

    Assignment-2 Submitted

---
+++ Assignment-2 LU.py
@@ -0,0 +1,12 @@
+name= input("Enter your name: ")
+age = int(input("Enter your age: "))
+marks = float(input("Enter your marks: "))
+address = input("Enter your address: ")
+salary = float(input("Enter your Salary:"))
+
+
+print(f"My name is {name}\
+ i am {age}yrs old\
+ scored {marks}\
+ lives in {address}\
+ and earns Rs-{salary}")
